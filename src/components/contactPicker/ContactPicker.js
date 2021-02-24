aa61629d2c64a921ced00b6f45797ef8880f4123
jva6 <jva6@gatech.edu>
2021-02-24 13:32:47 -0500

    Minor component change

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const ContactPicker = ({ name, onChange, contacts }) => {
   return (
     <select name={name} onChange={onChange}>
-      <option value={""} key={-1} selected="selected">No Contact Selected</option>
+      <option value={''} key={-1} selected="selected">
+        No Contact Selected
+      </option>
       {contacts.map((contact) => {
         return (
           <option value={contact} key={contact}>
