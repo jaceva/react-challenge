6f90815d094f04699b00e6f288ba987c3ffa9e96
jva6 <jva6@gatech.edu>
2021-03-04 14:47:18 -0500

    Modified AppointmentsPage.js

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,9 +1,6 @@
 import React from "react";
 
-import { ContactForm } from "../../components/contactForm/ContactForm";
-import { TileList } from "../../components/tileList/TileList";
-
-export const ContactsPage = ({ contacts, addContact }) => {
+export const ContactsPage = () => {
   /*
   Define state variables for 
   contact info and duplicate check
